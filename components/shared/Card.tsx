d51d5165ad01dafbf03fdf9660058be1855a304a
Ali Mohsin <108975862+Ali-Ch-001@users.noreply.github.com>
2024-07-17 16:56:10 +0500

    Fixed Typo

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,6 +1,6 @@
 import { IEvent } from '@/lib/database/models/event.model'
 import { formatDateTime } from '@/lib/utils'
-import { auth } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
